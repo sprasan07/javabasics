653738a7e77f42c980d4ac7e511be7757ed832ce
Prasanna <sprasan07@gmail.com>
2013-06-18 18:06:59 +0530

    Introduced Delish Problem class

---
+++ GitTest/src/org/prasanna/codechef/problems/Delish.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package org.prasanna.codechef.problems;
+
+/**
+ * @author Prasanna Srinivasan
+ *
+ */
+public class Delish {
+
+	/**
+	 * 
+	 */
+	public Delish() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
